1d3bf8adf6cc292f59ff15cb6baf5a008c033ef3
Maksim Ruts <gambitns@gmail.com>
2016-10-15 09:22:38 +0300

    initial commit

---
+++ src/main/java/com/epam/k/Application.java
@@ -0,0 +1,14 @@
+package com.epam.k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Maksim Ruts on 10/15/2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
