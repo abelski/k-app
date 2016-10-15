4dd78882a577a2bb7302e6ce9d81a1b76cf85a78
Ruslan_Mostov <ruslan_mostov@epam.com>
2016-10-15 15:13:55 +0300

    email is working

--- src/main/java/com/epam/k/configuration/WebConfiguration.java
+++ src/main/java/com/epam/k/configuration/WebConfiguration.java
@@ -1,6 +1,8 @@
 package com.epam.k.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -10,5 +12,18 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Bean
+    public JavaMailSenderImpl configureAndReturnSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost("smtp.gmail.com");
+        sender.setUsername("hackathonhere@gmail.com");
+        sender.setPassword("hackathon");
+        sender.setPort(587);
+        sender.getJavaMailProperties().setProperty("mail.mime.charset", "UTF-8");
+        sender.getJavaMailProperties().setProperty("mail.smtp.starttls.enable", "true");
+
+        return sender;
     }
 }
