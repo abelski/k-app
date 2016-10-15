e2b139d4625cef9d195aaca3a3ee78290f98069c
Nikita_Budziukhin <budziukhin.n@gmail.com>
2016-10-15 11:14:22 +0300

    VacationFilterPredicateBuilder added

--- src/main/java/com/epam/k/configuration/PersistenceConfiguration.java
+++ src/main/java/com/epam/k/configuration/PersistenceConfiguration.java
@@ -1,11 +1,15 @@
 package com.epam.k.configuration;
 
+import com.epam.k.filter.VacationFilterPredicateBuilder;
+import com.epam.k.filter.impl.*;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMongoRepositories(basePackages = {"com.epam.k.dao"})
@@ -17,5 +21,12 @@ public class PersistenceConfiguration {
     @Bean
     public MongoDatabase database(MongoClient client) {
         return client.getDatabase(mongoDb);
+    }
+
+    @Bean
+    public VacationFilterPredicateBuilder vacationFilterPredicateBuilder() {
+        VacationFilterPredicateBuilderImpl builder = new VacationFilterPredicateBuilderImpl();
+        builder.setFilters(Arrays.asList(new DatesFilter(), new OwnerIdFilter(), new TagsFilter(), new TransportFilter()));
+        return builder;
     }
 }
