a6c0ef9c505178d1b737e5a2862df7ca995e3469
yauhenl <yauhen.lisimau@gmail.com>
2016-10-15 10:20:14 +0300

    add basic security

--- src/main/java/com/epam/k/dao/UserDAO.java
+++ src/main/java/com/epam/k/dao/UserDAO.java
@@ -9,9 +9,6 @@ import com.epam.k.domain.User;
 /**
  * Created by Ruslan_Mostov on 10/15/2016.
  */
-public interface UserDAO extends PagingAndSortingRepository<User, String>, QueryDslPredicateExecutor<User>
-{
-   User findByUsername(String username);
-
-   User findByToken(String token);
+public interface UserDAO extends PagingAndSortingRepository<User, String>, QueryDslPredicateExecutor<User> {
+    User findByUsername(String username);
 }
