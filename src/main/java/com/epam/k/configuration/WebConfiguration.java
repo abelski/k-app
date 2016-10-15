7cf16f3a1ca1f4fcacce915f30627f15eb8dd0d7
Nikita_Budziukhin <budziukhin.n@gmail.com>
2016-10-15 12:27:34 +0300

    Added pics resource handler and pics folder generation script. Replaced /images property to /pics

--- src/main/java/com/epam/k/configuration/WebConfiguration.java
+++ src/main/java/com/epam/k/configuration/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.epam.k.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,5 +11,13 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/pics/**")
+                .addResourceLocations("/pics/");
     }
 }
