0b98f22e9e3f5b521754d0df56a2b7832e2e9616
Maksim Ruts <gambitns@gmail.com>
2016-10-15 12:48:57 +0300

    fixed: added SpyFilder for requests showing

--- src/main/java/com/epam/k/web/filter/SpyFilter.java
+++ src/main/java/com/epam/k/web/filter/SpyFilter.java
@@ -4,6 +4,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import java.util.Objects;
 /**
  * Created by Maksim Ruts on 10/15/2016.
  */
+@WebFilter
 public class SpyFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
