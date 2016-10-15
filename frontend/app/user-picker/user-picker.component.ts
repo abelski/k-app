5a8f3ace4b0c7de74ef2cc06b38c2e723063712d
Alexandr Kolymago <kolymago.a@gmail.com>
2016-10-15 10:08:40 +0300

    add user-picker

---
+++ frontend/app/user-picker/user-picker.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input } from '@angular/core';
+import { Vacation } from '../domain/vacation';
+import { UserService } from './user.service';
+import { User } from '../domain/user';
+declare var $: any;
+
+@Component({
+    selector: 'user-picker',
+    templateUrl: 'app/user-picker/user-picker.template.html',
+    providers: [UserService]
+})
+
+export class UserPickerComponent {
+    // private userList = ["Aaron A. Aaronson", "Nima Laszlo", "Khalil Seachnall", "Platon I'timad", "Sakhr Ranulf", "Valentin Haroun",
+    //     "Gallagher Baldur", "Jafar Aodhan", "Eyvindur Kajetan", "Einion Leary", "Augusto Kristof", "Nasir Miloslav", "Bearach Murchadh",
+    //     "Fechin Darragh", "Dionisio Maximiliano", "Aracely Woerner", "Silvia Giehl", "Eugenia Houston", "Ofelia Ali", "Þordis Petocs", "Bergljot Hanigan",
+    //     "Herodotus Adamsen", "Susann Danielsen", "Luciana Hummel", "Rasmus Hermansen", "Abraham Falk", "Theodoulos Steensen", "Grete Landvik", "Pomponia Carlson"];
+
+    private userList: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.userService.getAllUsers().subscribe(user => this.userList.push(user));
+        this.userList.sort();
+        // makes Case Insensitive 'contains'
+        $.extend($.expr[":"], {
+            "containsIN": function (elem, i, match, array) {
+                return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+            }
+        });
+    }
+
+    ngAfterViewInit() {
+        $("#user-filter-input").bind("input", function () {
+            var filterText = $(this).val();
+            $(".user-pick-item").removeClass("hidden");
+            $(".user-pick-item:not(:containsIN('" + filterText + "'))").addClass("hidden");
+        });
+        
+        $(".user-pick-item").click(function() {
+            $(this).toggleClass("selected");
+        });
+    }
+
+    public init() {
+        $("#user-filter-input").val("");
+        $(".user-pick-item").removeClass("hidden");
+    }
+}
---
+++ frontend/app/user-picker/user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { User } from '../domain/user';
+import { UrlUtil } from '../utils/url.util';
+
+@Injectable()
+export class UserService {
+
+    constructor(private http: Http) { }
+
+    public getAllUsers(): Observable<User> {
+        return this.http.get(UrlUtil.GET_ALL_USERS)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    public getUserById(id: number) {
+        return this.http.get(UrlUtil.GET_USER_BY_ID)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        let i: number = 0;
+        let users: User[] = [];
+        if (body.content) {
+            // if response has more than one vac
+            for (let user of body.content) {
+                users.push(JSON.parse(JSON.stringify(user)));
+            }
+        } else {
+            // if response has only one vac
+            return body;
+        }
+        return users || {};
+    }
+
+    private handleError(error: any) {
+        // we might use a remote logging
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+    }
+}
