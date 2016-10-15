4430aaf5c420aded6004576e3d87613d66d73146
yauhenl <yauhen.lisimau@gmail.com>
2016-10-15 10:27:36 +0300

    delete filter

--- src/main/java/com/epam/k/service/VacationService.java
+++ src/main/java/com/epam/k/service/VacationService.java
@@ -1,41 +1,31 @@
 package com.epam.k.service;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-
 import com.epam.k.dao.VacationDAO;
 import com.epam.k.domain.User;
 import com.epam.k.domain.Vacation;
 import com.epam.k.domain.enums.VacationStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 
-/**
- * Created by Ruslan_Mostov on 10/15/2016.
- */
-public class VacationService extends BaseService<Vacation, String>
-{
-   @Autowired
-   private VacationDAO vacationDAO;
+@Service
+public class VacationService extends BaseService<Vacation, String> {
+    @Autowired
+    private VacationDAO vacationDAO;
 
-   @Override
-   public VacationDAO getRepository()
-   {
-      return vacationDAO;
-   }
+    @Override
+    public VacationDAO getRepository() {
+        return vacationDAO;
+    }
 
-   @Override
-   public QueryDslPredicateExecutor<Vacation> getQueryDSLPredicateExecutor()
-   {
-      return vacationDAO;
-   }
+    @Override
+    public QueryDslPredicateExecutor<Vacation> getQueryDSLPredicateExecutor() {
+        return vacationDAO;
+    }
 
-   public Iterable<Vacation> findAllByOwner(final User user)
-   {
-      final List<Vacation> vacationsOwnedByUser = getRepository().findAllByOwnerAndStatusIn(user, Arrays.asList
-            (VacationStatus.OPEN, VacationStatus.CLOSED));
-      return vacationsOwnedByUser;
-   }
+    public Iterable<Vacation> findAllByOwner(final User user) {
+        return getRepository().findAllByOwnerAndStatusIn(user, Arrays.asList (VacationStatus.OPEN, VacationStatus.CLOSED));
+    }
 }
