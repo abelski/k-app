f8ff0a47c70d4bbdcf3dfaf1e744f65eab57deed
Nikita_Budziukhin <budziukhin.n@gmail.com>
2016-10-15 15:13:10 +0300

    Added db clean to application event listener

--- src/main/java/com/epam/k/listener/CreateVacationsListener.java
+++ src/main/java/com/epam/k/listener/CreateVacationsListener.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import com.mongodb.client.MongoDatabase;
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +25,15 @@ public class CreateVacationsListener implements ApplicationListener<ContextRefre
     @Autowired
     private VacationDAO vacationDAO;
 
+    @Autowired
+    private MongoDatabase mongoDatabase;
+
     @Value("${vacations.list}")
     private String vacationsPath;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        mongoDatabase.drop();
         try (InputStream vacationsStream = getClass().getClassLoader().getResourceAsStream(vacationsPath)) {
             String vacationsJson = IOUtils.toString(vacationsStream);
             ObjectMapper objectMapper = new ObjectMapper();
